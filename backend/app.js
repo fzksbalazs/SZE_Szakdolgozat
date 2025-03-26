942434b59e84aeee1676257012de55b41d2ca886
fzksbalazs <70636449+fzksbalazs@users.noreply.github.com>
2025-03-26 19:11:26 +0100

    Routeok, postman tesztelés

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
-const router = express.Router();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 var cors = require("cors");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
+const cartRoute = require("./routes/cart");
+const productRoute = require("./routes/product");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const emailRoutes = require("./routes/emailRoutes");
 
 
 dotenv.config(); //env fileból toltes
@@ -22,6 +28,14 @@ mongoose
   app.use(cors()); //Google miatt kell + frontend backend osszekotes
   app.use(express.json());
 
+  
+  app.use("/api/auth", authRoute);
+  app.use("/api/users", userRoute);
+  app.use("/api/products", productRoute);
+  app.use("/api/carts", cartRoute);
+  app.use("/api/orders", orderRoute);
+  app.use("/api/checkout", stripeRoute);
+  app.use("/api/emails", emailRoutes);
 
   app.listen(process.env.PORT || 5000, () => {
     console.log("Backend szerver fut!");
