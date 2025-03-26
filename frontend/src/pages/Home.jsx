4e303666367ec6a540b17e015309957dbfa7c843
fzksbalazs <70636449+fzksbalazs@users.noreply.github.com>
2025-03-26 19:17:40 +0100

    Alap frontend felallitasa

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Announcement from "../components/Announcement";
+
+
+const Home = () => {
+  return (
+    <div>
+      
+      <Announcement />
+      
+
+    </div>
+  );
+};
+
+export default Home;
