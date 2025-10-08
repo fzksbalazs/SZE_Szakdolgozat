c7345bdd67a7f7a267343540792be771979a9cce
fzksbalazs <70636449+fzksbalazs@users.noreply.github.com>
2025-10-08 17:58:57 +0200

    design bejefezése

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -28,11 +28,13 @@ import {
     display: flex;
     align-items: center;
     justify-content: center;
-    background-color: #f5fbfd;
+    background-color: white;
     position: relative;
+    border-radius: 16px;
   
     &:hover ${Info}{
       opacity: 1;
+      border-radius: 50px;
     }
   `;
   
@@ -40,14 +42,43 @@ import {
     width: 200px;
     height: 200px;
     border-radius: 50%;
-    background-color: white;
+    background: linear-gradient(-45deg, #0d0d0f, #1b1833, #3a2c72, #0d0d0f);
     position: absolute;
   `;
   
-  const Image = styled.img`
-    height: 75%;
-    z-index: 2;
-  `;
+// // const Image = styled.img`
+// //   width: 80%;
+// //   height: auto; /* a magasság automatikus az arány megtartásához */
+// //   max-height: 220px; /* ne legyen túl nagy egyik se */
+// //   object-fit: contain; /* nem torzítja a képet */
+// //   z-index: 2;
+// //   display: block;
+// //   margin: 0 auto; /* vízszintesen középre */
+// //   position: relative;
+  
+  
+// `;
+const ImageWrapper = styled.div`
+  width: 280px;
+  height: 280px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden; /* nem engedi kilógni a képet */
+  position: relative;
+  z-index: 2;
+`;
+
+const Image = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: contain;
+`;
+
+
+
+
+
   
   const Icon = styled.div`
     width: 40px;
@@ -64,12 +95,34 @@ import {
       transform: scale(1.1);
     }
   `;
+
+const Title = styled.span`
+  position: absolute;
+  top: 12px;
+  left: 50%;
+  transform: translateX(-50%);
+  font-family: "Montserrat", sans-serif;
+  font-size: 13px;
+  font-weight: 600;
+  color: black; /* halvány fehér */
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  z-index: 4;
+  pointer-events: none;
+  text-align: center;
+`;
+
+
+
   
   const Product = ({ item }) => {
     return (
       <Container>
+        <Title>{item.title}</Title>
         <Circle />
+        <ImageWrapper>
         <Image src={item.img} />
+       </ImageWrapper>
         <Info>
           <Icon>
             <Link to={`/product/${item._id}`}>
