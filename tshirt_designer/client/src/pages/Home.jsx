026bf70fdcb7815edc169bc4fa63d3d8977bd7c3
fzksbalazs <70636449+fzksbalazs@users.noreply.github.com>
2025-10-16 17:04:59 +0200

    Update Home.jsx

--- tshirt_designer/client/src/pages/Home.jsx
+++ tshirt_designer/client/src/pages/Home.jsx
@@ -15,7 +15,8 @@ const Home = () => {
     const snap = useSnapshot(state);
 
      const handleLogoClick = () => {
-        window.location.href = 'http://localhost:3000';  // Helyi főoldalra irányít
+        window.history.pushState({}, '', '/');
+    window.location.reload();   // Helyi főoldalra irányít
     };
     return (
         <AnimatePresence>
