3fff2d29ec1e091614900326ee2e790ae4b9c40b
fzksbalazs <70636449+fzksbalazs@users.noreply.github.com>
2025-10-16 21:27:17 +0200

    Update Success.jsx

--- frontend/src/pages/Success.jsx
+++ frontend/src/pages/Success.jsx
@@ -1,17 +1,18 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useLocation } from "react-router";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useLocation, useHistory } from "react-router-dom";
 import { clearCart } from "../redux/cartRedux";
 import { userRequest } from "../requestMethods";
+import styled, { keyframes } from "styled-components";
 
 const Success = () => {
   const location = useLocation();
-  const data = location.state.stripeData;
+  const data = location.state?.stripeData;
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
   const currentUser = useSelector((state) => state.user.currentUser);
   const [orderId, setOrderId] = useState(null);
+  const [progress, setProgress] = useState(0);
   const history = useHistory();
 
   const handleDelete = (e) => {
@@ -21,12 +22,6 @@ const Success = () => {
   };
 
   useEffect(() => {
-    console.log("Current User:", currentUser);
-    console.log(
-      "Token in userRequest:",
-      userRequest.defaults.headers.Authorization,
-    );
-
     const createOrder = async () => {
       try {
         const res = await userRequest.post("/orders", {
@@ -38,37 +33,94 @@ const Success = () => {
           amount: cart.total,
           address: data.billing_details.address,
         });
-        console.log("Order response:", res.data);
         setOrderId(res.data._id);
+        startProgress();
       } catch (err) {
         console.log("Error creating order:", err);
       }
     };
-
-    if (data) {
-      createOrder();
-    }
+    if (data) createOrder();
   }, [cart, data, currentUser]);
 
-  return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      {orderId
-        ? `A rendelésed sikeres! A rendelés azonositod : ${orderId} `
-        : ``}
+  const startProgress = () => {
+    let value = 0;
+    const interval = setInterval(() => {
+      value += 1;
+      setProgress(value);
+      if (value >= 100) clearInterval(interval);
+    }, 20);
+  };
 
-      <button onClick={handleDelete} style={{ padding: 10, marginTop: 20 }}>
-        Menjünk a főoldalra
-      </button>
-    </div>
+  return (
+    <Container>
+      {orderId ? (
+        <>
+          <Message>
+            A rendelésed sikeres! <br /> Rendelés azonosító: <strong>{orderId}</strong>
+          </Message>
+
+          <ProgressBar>
+            <ProgressFill style={{ width: `${progress}%` }} />
+          </ProgressBar>
+
+          {progress === 100 && (
+            <Button onClick={handleDelete}>Menjünk a főoldalra</Button>
+          )}
+        </>
+      ) : (
+        <Message>Rendelés feldolgozása...</Message>
+      )}
+    </Container>
   );
 };
 
 export default Success;
+
+// Styled components
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  
+  color: white;
+`;
+
+const Message = styled.div`
+  font-size: 20px;
+  text-align: center;
+  margin-bottom: 30px;
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  margin-top: 20px;
+  font-weight: bold;
+  cursor: pointer;
+  background-color: black;
+  color: black;
+  border: none;
+  border-radius: 5px;
+  color: white;
+  transition: 0.3s;
+  &:hover {
+    background-color: white;
+    color: black;
+  }
+`;
+
+const ProgressBar = styled.div`
+  width: 300px;
+  height: 15px;
+  border: 2px solid white;
+  border-radius: 10px;
+  overflow: hidden;
+ 
+`;
+
+const ProgressFill = styled.div`
+  height: 100%;
+  background: linear-gradient(90deg, #00ff7f, #32cd32);
+  transition: width 0.2s;
+`;
