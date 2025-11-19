1c5eb15c50b48f4684b9823b2fe201ad12c552e8
fzksbalazs <70636449+fzksbalazs@users.noreply.github.com>
2025-11-19 15:16:09 +0100

    Update requestMethods.js

--- frontend/src/requestMethods.js
+++ frontend/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-  const BASE_URL = "https://wearable-rira.onrender.com/api/";
+const BASE_URL = "https://wearable-rira.onrender.com/api/";
 
  //const BASE_URL = "http://localhost:5000/api/";
 
