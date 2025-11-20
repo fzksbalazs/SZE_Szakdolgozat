d7f38780cade8cf31cc5b97e07b67ac9f69bcaa3
fzksbalazs <70636449+fzksbalazs@users.noreply.github.com>
2025-11-20 20:59:30 +0100

    Update Customizer.jsx

--- tshirt_designer/client/src/pages/Customizer.jsx
+++ tshirt_designer/client/src/pages/Customizer.jsx
@@ -127,7 +127,7 @@ const Customizer = ({ productId }) => {
 
     try {
       setgeneratingImg(true);
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+      const response = await fetch("https://ai-szerver.onrender.com/api/v1/dalle", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ prompt }),
